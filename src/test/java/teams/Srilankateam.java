06f9d01b22fe40684e61e76d2c5f42f1d479a0fc
shubhammhadaye <shubhammhadaye97@gmail.com>
2023-07-12 23:36:02 +0530

    [shubham] srilanka team created

---
+++ src/test/java/teams/Srilankateam.java
@@ -0,0 +1,10 @@
+package teams;
+
+public class Srilankateam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("srilanka team created");
+	}
+
+}
